d2cadf2e66ef3f6bb20bf85c9fb4dafdeaabf3c1
diegoveega <00148816@uca.edu.sv>
2019-04-05 07:19:23 -0600

    initial

---
+++ app/src/main/java/com/diegoveega/laboratorio4_pdm_00148816/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.diegoveega.laboratorio4_pdm_00148816
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
